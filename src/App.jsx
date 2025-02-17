395b03bae803a61689806050b03c0c186cc1d7ec
Robin Mukherjee <robin@otaking.co.uk>
2025-02-17 17:03:58 +0000

    fix case

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import User from './pages/User'
